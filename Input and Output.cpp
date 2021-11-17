ebbf57dc5ce4e94bb58392d83a8739b4aa5e1638
Stefanus Ferry Susanto <91201205+FerryJr@users.noreply.github.com>
2021-11-17 10:55:26 +0700

    Add files via upload

---
+++ Input and Output.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int sum;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    
+    sum = a+b+c;
+    cout<<sum;
+      
+    return 0;
+}
+
